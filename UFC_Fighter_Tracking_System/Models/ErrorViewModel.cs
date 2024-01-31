1021a5cee852ead552ac94b3c135b18049c9f321
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-31 13:40:30 -0600

    rename files and folders for better structure organization of project

--- UFC_Fighter_Tracking_System/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace UFC_Fighter_Tracking_System.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
