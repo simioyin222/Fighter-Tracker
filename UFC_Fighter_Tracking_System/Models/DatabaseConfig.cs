4c06f4cc02d01ca60d7c08aa6e821e19c439bb13
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-31 17:52:26 -0600

    connecting database to ASP.NET core app

---
+++ UFC_Fighter_Tracking_System/Models/DatabaseConfig.cs
@@ -0,0 +1,7 @@
+namespace UFC_Fighter_Tracking_System.Models
+{
+    public static class DBConfiguration
+    {
+        public static string? ConnectionString { get; set; }
+    }
+}
--- UFC_Fighter_Tracking_System/Program.cs
+++ UFC_Fighter_Tracking_System/Program.cs
@@ -1,27 +1,45 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using UFC_Fighter_Tracking_System.Models;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace UFC_Fighter_Tracking_System
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            
+            builder.Services.AddControllersWithViews();
+
+           
+            DBConfiguration.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            var app = builder.Build();
+
+            
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
